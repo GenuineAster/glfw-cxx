4963ebf386b4af0af02ad99270584996db3b2e7f
Mischa-Alff <mischa.alff@gmail.com>
2014-02-13 13:36:33 +0100

    Input: Added MouseButton enum

--- include/glfw-cxx/Input.hpp
+++ include/glfw-cxx/Input.hpp
@@ -135,4 +135,21 @@ namespace glfw
 		Menu,
 		Last = Menu
 	};
+
+	enum MouseButton
+	{
+		Button1 = 0,
+		Button2,
+		Button3,
+		Button4,
+		Button5,
+		Button6,
+		Button7,
+		Button8,
+		ButtonLast = Button8,
+
+		ButtonLeft = Button1,
+		ButtonRight = Button2,
+		ButtonMiddle = Button3
+	};
 }
